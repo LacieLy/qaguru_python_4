4180f0c40e53b6df2c7bf7736cbb32144c551e45
DSpiridonova <Dafna.svetlova.83@gmail.com>
2022-07-26 17:26:47 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -14,3 +14,21 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     pass
 
+
+def no_arguments_test():
+    pass
+
+
+def print_readable_names(names):
+    function_name = names.__name__
+    arguments_name = names.__code__.co_varnames
+    print('Function: ', *function_name.capitalize().replace("_", " "), sep='')
+    if len(arguments_name) > 0:
+        print('Argument: ', *arguments_name, '\n')
+    else:
+        print("The function has no arguments")
+
+
+list_func = [open_browser, go_to_companyname_homepage, find_registration_button_on_login_page, no_arguments_test]
+for names in list_func:
+    print_readable_names(names)
